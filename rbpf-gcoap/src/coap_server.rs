0102c857de967a77d6f1b560f913c855dee9e45b
SzymonKubica <szymo.kubica@gmail.com>
2024-02-12 10:30:43 +0100

    Refactored the main server.

--- rbpf-gcoap/src/coap_server.rs
+++ rbpf-gcoap/src/coap_server.rs
@@ -2,56 +2,58 @@ use coap_handler_implementations::SimpleRendered;
 use coap_message::{MessageOption, MutableWritableMessage, ReadableMessage};
 use core::convert::TryInto;
 use riot_wrappers::cstr::cstr;
-use riot_wrappers::{gcoap, gnrc, mutex::Mutex, riot_sys, stdio::println, thread, ztimer};
+use riot_wrappers::{
+    gcoap, coap_handler::GcoapHandler, gcoap::SingleHandlerListener, gnrc, mutex::Mutex, riot_sys,
+    stdio::println, thread, ztimer,
+};
 
-use crate::handlers::{handle_benchmark, handle_femtocontainer_execution_on_coap_packet, handle_rbpf_execution_on_coap_packet};
 use crate::handlers::handle_bytecode_load;
 use crate::handlers::handle_console_write;
 use crate::handlers::handle_suit_pull;
+use crate::handlers::{
+    handle_benchmark, handle_femtocontainer_execution_on_coap_packet,
+    handle_rbpf_execution_on_coap_packet,
+};
 use crate::handlers::{handle_femtocontainer_execution, handle_riot_board};
 
 pub fn gcoap_server_main(_countdown: &Mutex<u32>) -> Result<(), ()> {
     // Each endpoint needs a request handler defined as its own struct implemneting
     // the Handler trait. Then we need to initialise a listener for that endpoint
     // and add it as a resource in the gcoap scope.
-    let mut console_write_handler =
-        riot_wrappers::coap_handler::GcoapHandler(handle_console_write());
-    let mut console_write_listener = gcoap::SingleHandlerListener::new(
+    let mut console_write_handler = GcoapHandler(handle_console_write());
+    let mut riot_board_handler = GcoapHandler(handle_riot_board());
+    let mut bytecode_handler = handle_rbpf_execution_on_coap_packet();
+    let mut bench_handler = GcoapHandler(handle_benchmark());
+    let mut suit_pull_handler = GcoapHandler(handle_suit_pull());
+
+    let mut console_write_listener = SingleHandlerListener::new(
         cstr!("/console/write"),
         riot_sys::COAP_POST,
         &mut console_write_handler,
     );
 
-    let mut riot_board_handler = riot_wrappers::coap_handler::GcoapHandler(handle_riot_board());
-    let mut riot_board_listener = gcoap::SingleHandlerListener::new(
+    let mut riot_board_listener = SingleHandlerListener::new(
         cstr!("/riot/board"),
         riot_sys::COAP_GET,
         &mut riot_board_handler,
     );
 
-    let mut bytecode_handler = handle_rbpf_execution_on_coap_packet();
-    let mut bytecode_listener = gcoap::SingleHandlerListener::new(
+    let mut bytecode_listener = SingleHandlerListener::new(
         cstr!("/rbpf/exec"),
         riot_sys::COAP_POST,
         &mut bytecode_handler,
     );
 
-    let mut femtocontainer_handler = handle_femtocontainer_execution_on_coap_packet();
-    let mut femtocontainer_listener = gcoap::SingleHandlerListener::new(
+    let mut femtocontainer_listener = SingleHandlerListener::new(
         cstr!("/femto-container/exec"),
         riot_sys::COAP_POST,
         &mut femtocontainer_handler,
     );
 
-    let mut bench_handler = riot_wrappers::coap_handler::GcoapHandler(handle_benchmark());
-    let mut bench_listener = gcoap::SingleHandlerListener::new(
-        cstr!("/benchmark"),
-        riot_sys::COAP_POST,
-        &mut bench_handler,
-    );
+    let mut bench_listener =
+        SingleHandlerListener::new(cstr!("/benchmark"), riot_sys::COAP_POST, &mut bench_handler);
 
-    let mut suit_pull_handler = riot_wrappers::coap_handler::GcoapHandler(handle_suit_pull());
-    let mut suit_pull_listener = gcoap::SingleHandlerListener::new(
+    let mut suit_pull_listener = SingleHandlerListener::new(
         cstr!("/suit/pull"),
         riot_sys::COAP_POST,
         &mut suit_pull_handler,
@@ -73,7 +75,6 @@ pub fn gcoap_server_main(_countdown: &Mutex<u32>) -> Result<(), ()> {
         let sectimer = ztimer::Clock::sec();
         sectimer.sleep_ticks(2);
 
-
         // Sending main thread to sleep; can't return or the Gcoap handler would need to be
         // deregistered (which it can't).
         loop {
@@ -85,22 +86,21 @@ pub fn gcoap_server_main(_countdown: &Mutex<u32>) -> Result<(), ()> {
 }
 
 fn print_network_interfaces() {
-
-        for netif in gnrc::Netif::all() {
-            println!(
-                "Active interface from PID {:?} ({:?})",
-                netif.pid(),
-                netif.pid().get_name().unwrap_or("unnamed")
-            );
-            match netif.ipv6_addrs() {
-                Ok(addrs) => {
-                    for a in &addrs {
-                        println!("    Address {:?}", a);
-                    }
-                }
-                _ => {
-                    println!("    Does not support IPv6.");
+    for netif in gnrc::Netif::all() {
+        println!(
+            "Active interface from PID {:?} ({:?})",
+            netif.pid(),
+            netif.pid().get_name().unwrap_or("unnamed")
+        );
+        match netif.ipv6_addrs() {
+            Ok(addrs) => {
+                for a in &addrs {
+                    println!("    Address {:?}", a);
                 }
             }
+            _ => {
+                println!("    Does not support IPv6.");
+            }
         }
+    }
 }
